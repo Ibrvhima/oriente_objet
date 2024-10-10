2c50a7d2d36e228da03820cf8977443b1ba08f07
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-10-10 11:23:11 +0000

    creation d'une fonction constructive en POO

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+//création des fonction constructive
+const Person = function(firstName, brithYear){}
+
+//exécution de la fonction constructive on use l'operateur "new"
+
+new Person("Diallo", 2000)
