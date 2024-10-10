a727bc7d1e3ebcca5ee5b9cd113eb180078d4d22
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-10-10 11:36:28 +0000

    creation d'instance

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 "use strict";
 //création des fonction constructive
-const Person = function(firstName, brithYear){}
+const Person = function (firstName, brithYear) {
+  this.firstName = firstName;
+  this.brithYear = brithYear;
+};
+const Amad = new Person("Amadou", 2000);
+const Oumar = new Person("Oumar", 2000);
+const Conte = new Person("conte", 2000);
+
+
+console.log(Amad);
+console.log(Oumar);
+console.log(Conte);
 
 //exécution de la fonction constructive on use l'operateur "new"
 
-new Person("Diallo", 2000)
+new Person("Diallo", 2000);
