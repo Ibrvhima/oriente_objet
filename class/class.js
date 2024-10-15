e84f2a0679736a3f25f9b70d9bb275d4cbc861f8
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-10-15 10:29:36 +0000

    a new commit

--- class/class.js
+++ class/class.js
@@ -1,24 +1,43 @@
 "use strict";
 
 class Account {
+  _movement = [];
+  locale = navigator.language;
+
   constructor(owner, currency, pin, movement) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movement = [];
-    this.locale = navigator.language;
+    //protected property
+    this._pin = pin;
 
     console.log(`Thanks for opening an account ${this.owner}`);
   }
-
-  deposit(value) {
-    this.movement.push(value);
+  getMovement() {
+    return this._movement;
   }
+  //DEPOT
+  deposit(value) {
+    this._movement.push(value);
+  }
+
+  //RETRAIT
   withdrawa(value) {
     this.deposit(-value);
+  }
+  //PRETER DE L'ARGENT
+  approveLoan(value) {
+    return true;
+  }
+  resquestLoan(value) {
+    if (this.approveLoan(value)) {
+      this.deposit(value);
+      console.log("Loan approuved");
+    }
   }
 }
 const acc1 = new Account("Jullien", "GNF", 111);
 
-acc1.movement.push(250);
-acc1.movement.push(-100);
+acc1.deposit(250);
+acc1.withdrawa(100);
+acc1.getMovement.push();
+acc1._movement.push(-100);
